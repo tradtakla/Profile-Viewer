fe3ae0177e63acabcc762c454285a977bef143f1
Takla Trad <takla.trad@outlook.com>
2017-08-31 15:15:39 +0300

    initial commit

---
+++ app/src/main/java/com/appsandgamesinc/progetter/ScanningActivity.java
@@ -0,0 +1,19 @@
+package com.appsandgamesinc.progetter;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+
+public class ScanningActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanning);
+
+//        SearchView searchview = (SearchView) findViewById(R.id.searchView);
+    }
+}
